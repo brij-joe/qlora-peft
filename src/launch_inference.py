15e293f937b7e8fce0905b8e7c80043cd778e823
brij_joe <brij_joe@yahoo.com>
2026-03-29 23:54:30 +0530

    updated inference file

--- src/launch_inference.py
+++ src/launch_inference.py
@@ -1,29 +1,40 @@
+import os
+
+from dotenv import load_dotenv
 from transformers import BitsAndBytesConfig
 import torch
 
+from config.qlora_config import QLoRAConfig
 from model.qlora_inference import QLoRAInference
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4"
-)
+if __name__ == "__main__":
+    load_dotenv(verbose=True)
+    config = QLoRAConfig()
+    config.model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    config.hf_token = os.environ["HF_TOKEN"]
 
-# First call → initializes model
-inference = QLoRAInference(
-    model_name="mistralai/Mistral-7B-Instruct-v0.3",
-    adapter_path="./qlora-adapter",
-    bnb_config=bnb_config
-)
+    bnb_config = BitsAndBytesConfig(
+        torch_dtype = torch.float16,  # instead of 4bit
+        # load_in_4bit = self.config._load_in_4bit,
+        bnb_4bit_compute_dtype = self.config._bnb_compute_dtype,
+        bnb_4bit_use_double_quant = self.config._bnb_use_double_quant,
+        bnb_4bit_quant_type = self.config._bnb_quant_type,
+        llm_int8_enable_fp32_cpu_offload = self.config._cpu_offload, )
 
-response = inference.generate("Explain LoRA in simple terms")
-print(response)
+    # First call → initializes model
+    inference = QLoRAInference(
+        model_name = config.model_name,
+        adapter_path = "./qlora-adapter",
+        bnb_config = bnb_config
+    )
 
-# Second call → reuses same instance (no reload 🚀)
-inference2 = QLoRAInference(
-    model_name="ignored",
-    adapter_path="ignored"
-)
+    response = inference.generate("Explain LoRA in simple terms")
+    print(response)
 
-print(inference is inference2)  # True
+    # Second call → reuses same instance (no reload 🚀)
+    inference2 = QLoRAInference(
+        model_name = "ignored",
+        adapter_path = "ignored"
+    )
+
+    print(inference is inference2)  # True
